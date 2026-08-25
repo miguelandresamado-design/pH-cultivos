7796130330dfbaa3d6352ee587b2062c20c38727
miguelandresamado-design <miguelandresamado@gmail.com>
2026-08-25 11:57:08 -0500

    Actualizar caché PWA a versión 6

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ph-cultivos-v5';
+const CACHE_NAME='ph-cultivos-v6';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS.map(asset=>new Request(asset,{cache:'reload'})))));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
