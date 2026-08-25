b2e8776be1cf56ad30958e568510ce36e8ec3e04
miguelandresamado-design <miguelandresamado@gmail.com>
2026-08-25 09:20:25 -0500

    Actualizar caché PWA a versión 2

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ph-cultivos-v1';
+const CACHE_NAME='ph-cultivos-v2';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS)));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
