0389430b6466ea00d06a5981573d11bb692ffbc0
miguelandresamado-design <miguelandresamado@gmail.com>
2026-08-28 13:09:26 -0500

    Actualizar caché para versión 3.5

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='ph-cultivos-v7';
+const CACHE_NAME='ph-cultivos-v8';
 const ASSETS=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>{event.waitUntil(caches.open(CACHE_NAME).then(cache=>cache.addAll(ASSETS.map(asset=>new Request(asset,{cache:'reload'})))));self.skipWaiting();});
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE_NAME).map(k=>caches.delete(k)))));self.clients.claim();});
